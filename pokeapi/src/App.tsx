c0e77198e6ad1a5f887b3175abb8cde79775c8aa
MauricioLsalles <mauricio.leano@jala.university>
2025-05-29 15:21:11 -0400

    fetch

--- pokeapi/src/App.tsx
+++ pokeapi/src/App.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react'
 import PokemonCard from './Organisms/PokemonCard/PokemonCard'
 
 function App() {
 
+  const [number, setNumner] = useState("");
+  const [name, setName] = useState("");
+  const [labels, setLabels] = useState(["grass", "poison"]);
+  const [imgSrc, setImgSrc] = useState("");
+
+  useEffect(() => {
+     loadPokemon();
+  }, [])
+
+  async function loadPokemon() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    const json = await res.json();
+    setName(json.name);
+    setNumner(json.id);
+    setImgSrc(json.sprites.back_default)
+    console.log(json);
+  }
+  
+
   return (
     <>
-      <PokemonCard number={"001"} name={"Bulbasor"} labels={["grass", "poison"]} imgSrc={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png'}/>
+      <PokemonCard number={number} name={name} labels={labels} imgSrc={imgSrc}/>
     </>
   )
 }
 
 export default App
+
+
