19c0672d368827d69d4b28338568679b29a92ea9
MauricioLsalles <mauricio.leano@jala.university>
2025-06-02 22:59:59 -0400

    refactor hooks

--- pokeapi/src/CustomHooks/LoadPokemonsHook.ts
+++ pokeapi/src/CustomHooks/LoadPokemonsHook.ts
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import type { Pokemon } from "../Types/Pokemon";
+import type { PokemonListsResponse } from "../Types/PokemonListsResponse";
 
 function LoadPokemonsHook():Pokemon[]{
 
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+  async function load(){
+    const pokeList:Pokemon[]|undefined =  await loadList();
+    if(pokeList) setPokemons(pokeList);
+    console.log(pokeList);
+  }
+
   useEffect(() => {
-        loadList();
+        load();
     }, [])
 
   return pokemons;
@@ -16,26 +23,39 @@ export default LoadPokemonsHook;
 
 
 
-async function loadPokemon() {
-  const newPokemon:Pokemon = {
-    name: "",
-    number:"",
-    imgSrc:"",
-    types:[]
-  };
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-  const json = await res.json();
-  newPokemon.name = json.name;
-  newPokemon.number = json.id;
-  newPokemon.imgSrc = json.sprites.back_default;
-  newPokemon.types = json.types;
+async function loadPokemon(url:string) {
+  try{
+    const newPokemon:Pokemon = {
+      name: "",
+      number:"",
+      imgSrc:"",
+      types:[]
+    };
+    const res = await fetch(url);
+    const json = await res.json();
+    newPokemon.name = json.name;
+    newPokemon.number = json.id;
+    newPokemon.imgSrc = json.sprites.back_default;
+    newPokemon.types = json.types;
+    return newPokemon;
+  }
+  catch{
+    console.log("error");
+  }
 }
 
 async function loadList() {
+  const pokemonList:Pokemon[] = [];
+  try{
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
-    const json = await res.json();
-    console.log(json);
+    const pokeListRes:PokemonListsResponse = await res.json();
+    for(let pokeUrl of pokeListRes.result){
+      let pokemon:Pokemon|undefined = await loadPokemon(pokeUrl.url);
+      if(pokemon)  pokemonList.push(pokemon);
+    }
+    return pokemonList;
   }
-
-
-  
+  catch{
+    console.log("error");
+  }
+  }
