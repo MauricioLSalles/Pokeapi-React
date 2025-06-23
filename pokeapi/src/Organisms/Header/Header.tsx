cdb790805ef0982819033761249aa414a48cbe58
MauricioLsalles <mauricio.leano@jala.university>
2025-06-23 09:55:39 -0400

    header created

--- pokeapi/src/Organisms/Header/Header.tsx
+++ pokeapi/src/Organisms/Header/Header.tsx
@@ -1,3 +1,5 @@
+import HeaderTitle from "../../Atoms/HeaderTitle/HeaderTitle";
+import HeaderLinks from "../../Molecules/HeaderLinks/HeaderLinks";
 import type { Link } from "../../Types/Link";
 
 type CustomProps = {
@@ -6,13 +8,12 @@ type CustomProps = {
 
 type Props = CustomProps & React.ButtonHTMLAttributes<HTMLDivElement>;
 
-export default function HeaderLinks(props:Props) {
+export default function Header(props:Props) {
   const {links, ...ElementProps} = props;
   return (
     <div {...ElementProps}>
-        {links.map((link:Link) => 
-            <span>{link.name}</span>
-        )}
+        <HeaderTitle text="Pokedex"/>
+        <HeaderLinks links={links}/>
     </div>
   )
 }
