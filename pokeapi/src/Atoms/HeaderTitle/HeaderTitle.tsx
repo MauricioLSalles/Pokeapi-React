59c624cb9c5fe703f1373a83407bbaa08db901fb
MauricioLsalles <mauricio.leano@jala.university>
2025-06-23 10:15:14 -0400

    added style to header title

--- pokeapi/src/Atoms/HeaderTitle/HeaderTitle.tsx
+++ pokeapi/src/Atoms/HeaderTitle/HeaderTitle.tsx
@@ -9,8 +9,8 @@ type Props = CustomProps & React.ButtonHTMLAttributes<HTMLDivElement>;
 function HeaderTitle(props: Props): ReactElement {
   const { text, ...TitleProps } = props;
   return (
-    <div {...TitleProps}>
-      <img src={"/icons/pokeball.svg"} className='HeaderTitle-icon' />
+    <div style={{ display: 'flex', alignItems: 'center' }} {...TitleProps}>
+      <img src={"/icons/pokeball.png"} className='HeaderTitle-icon' />
       <span className="header-title">
         {text}
       </span>
