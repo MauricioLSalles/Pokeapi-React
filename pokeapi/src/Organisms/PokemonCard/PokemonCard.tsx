43145c13ef382747800e692b5973a87109f462a8
MauricioLsalles <mauricio.leano@jala.university>
2025-06-25 17:04:09 -0400

    refactor list

--- pokeapi/src/Organisms/PokemonCard/PokemonCard.tsx
+++ pokeapi/src/Organisms/PokemonCard/PokemonCard.tsx
@@ -1,23 +1,38 @@
-import './PokemonCard.css'
-import CardDescription from "../../Molecules/CardDescription/CardDescription"
-import MultipleLabels from '../../Molecules/MultipleLabels/MultipleLabels'
-import type { Label } from '../../Types/Label'
+import "./PokemonCard.css";
+import CardDescription from "../../Molecules/CardDescription/CardDescription";
+import MultipleLabels from "../../Molecules/MultipleLabels/MultipleLabels";
+import type { Label } from "../../Types/Label";
 
-
-function PokemonCard({number,name,labels, imgSrc}:{number:string,name:string,labels:Label[],imgSrc:string}) {
+function PokemonCard({
+  number,
+  name,
+  labels,
+  imgSrc,
+}: {
+  number: string;
+  name: string;
+  labels: Label[];
+  imgSrc: string;
+}) {
   return (
-    <div className="PokemonCard">
-      <img className='PokemonCardDots' src='/backgrounds/Pattern.png'/>
-      <div className='PokemonCard-description'>
-          <CardDescription number={number} name={name}/>
-          <MultipleLabels labels={labels}/>
+    <div
+      style={{ backgroundColor: `var(--type-${labels[0].type.name}` }}
+      className="PokemonCard"
+    >
+      <img className="PokemonCardDots" src="/backgrounds/Pattern.png" />
+      <div className="PokemonCard-description">
+        <CardDescription number={number} name={name} />
+        <MultipleLabels labels={labels} />
       </div>
-      <div className='PokemonCard-image-container'>
-        <img src={"/backgrounds/pokeball.png"} className='PokemonCard-image-background'/>
-        <img src={imgSrc} className='PokemonCard-image'/>
+      <div className="PokemonCard-image-container">
+        <img
+          src={"/backgrounds/pokeball.png"}
+          className="PokemonCard-image-background"
+        />
+        <img src={imgSrc} className="PokemonCard-image" />
       </div>
     </div>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
