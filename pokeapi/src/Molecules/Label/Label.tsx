bdf2d0594c297683b8dd4a46822e9d3f8b3cbf27
MauricioLsalles <mauricio.leano@jala.university>
2025-07-02 10:47:56 -0400

    refactor: normalize text sizes with classes

--- pokeapi/src/Molecules/Label/Label.tsx
+++ pokeapi/src/Molecules/Label/Label.tsx
@@ -4,7 +4,7 @@ import "./Label.css";
 function Label({ label }: { label: string }) {
   console.log(label)
   return (
-    <div style={{ backgroundColor: `var(--type-${label})` }} className="label">
+    <div style={{ backgroundColor: `var(--type-${label})` }} className="label text-normal">
       <img src={`/typesIcons/${label}.svg`} className="label-icon"></img>
       <LabelText label={label} />
     </div>
