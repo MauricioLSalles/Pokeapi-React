6c23874f0b0082c847e3a0f16d6c3ad7d016ece4
MauricioLsalles <mauricio.leano@jala.university>
2025-07-04 15:19:26 -0400

    feat: pokemon mask

--- pokeapi/src/Organisms/Game/Game.tsx
+++ pokeapi/src/Organisms/Game/Game.tsx
@@ -1,17 +1,29 @@
-import { type ReactElement } from 'react'
-import Button from '../../Atoms/Button/Button'
-import './Game.css'
+import { type ReactElement } from "react";
+import Button from "../../Atoms/Button/Button";
+import "./Game.css";
+import type { Response } from "../../Types/Response";
+import UseGet from "../../CustomHooks/UseGet";
+import type { Pokemon } from "../../Types/Pokemon";
 
 export default function Game(): ReactElement {
-    return (
-        <div className='game'>
-            <img className='guessPokemon' src='/placeholder/guessPlaceholder.png' />
-            <div className='gameButtons'>
-                <Button type='primary' text='Pikachu' />
-                <Button type='primary' text='Pikachu' />
-                <Button type='primary' text='Pikachu' />
-                <Button type='primary' text='Pikachu' />
-            </div>
-        </ div>
-    )
+  const response: Response<Pokemon> = UseGet<Pokemon>(
+    "https://pokeapi.co/api/v2/pokemon/1/"
+  );
+  if (response.loading) return <div>loading ...</div>;
+  return (
+    <div className="game">
+      <div
+        className="guessPokemon"
+        style={{ mask: `url(${response.data?.sprites.front_default})` }}
+      >
+        <div className="guessPokemonBackground" />
+      </div>
+      <div className="gameButtons">
+        <Button type="primary" text="Pikachu" />
+        <Button type="primary" text="Pikachu" />
+        <Button type="primary" text="Pikachu" />
+        <Button type="primary" text="Pikachu" />
+      </div>
+    </div>
+  );
 }
