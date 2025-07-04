f6e7e4600603fd03c8c26f57c39c70e6bc76ea89
MauricioLsalles <mauricio.leano@jala.university>
2025-07-04 16:00:05 -0400

    feat: transition when reveal

--- pokeapi/src/Organisms/Game/Game.tsx
+++ pokeapi/src/Organisms/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { type ReactElement } from "react";
+import { useRef, type ReactElement } from "react";
 import Button from "../../Atoms/Button/Button";
 import "./Game.css";
 import type { Response } from "../../Types/Response";
@@ -9,20 +9,28 @@ export default function Game(): ReactElement {
   const response: Response<Pokemon> = UseGet<Pokemon>(
     "https://pokeapi.co/api/v2/pokemon/1/"
   );
+
+  const imageRef = useRef<HTMLImageElement | null>(null);
+
+  function reveal() {
+    if (imageRef.current) {
+      imageRef.current.className = "guessPokemonImage show";
+    }
+  }
+
   if (response.loading) return <div>loading ...</div>;
   return (
     <div className="game">
-      <div
-        className="guessPokemon"
-        style={{ mask: `url(${response.data?.sprites.front_default})` }}
-      >
-        <div className="guessPokemonBackground" />
-      </div>
+      <img
+        ref={imageRef}
+        src={response.data?.sprites.front_default}
+        className={`guessPokemonImage `}
+      />
       <div className="gameButtons">
-        <Button type="primary" text="Pikachu" />
-        <Button type="primary" text="Pikachu" />
-        <Button type="primary" text="Pikachu" />
-        <Button type="primary" text="Pikachu" />
+        <Button onClick={reveal} text="Pikachu" />
+        <Button text="Pikachu" />
+        <Button text="Pikachu" />
+        <Button text="Pikachu" />
       </div>
     </div>
   );
