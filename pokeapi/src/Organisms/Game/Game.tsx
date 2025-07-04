452e36751c1cb4e63c521f149d06631f9c1761db
MauricioLsalles <mauricio.leano@jala.university>
2025-07-04 16:08:16 -0400

    refactor: added multiple pokemons

--- pokeapi/src/Organisms/Game/Game.tsx
+++ pokeapi/src/Organisms/Game/Game.tsx
@@ -9,6 +9,15 @@ export default function Game(): ReactElement {
   const response: Response<Pokemon> = UseGet<Pokemon>(
     "https://pokeapi.co/api/v2/pokemon/1/"
   );
+  const response1: Response<Pokemon> = UseGet<Pokemon>(
+    "https://pokeapi.co/api/v2/pokemon/2/"
+  );
+  const response2: Response<Pokemon> = UseGet<Pokemon>(
+    "https://pokeapi.co/api/v2/pokemon/3/"
+  );
+  const response3: Response<Pokemon> = UseGet<Pokemon>(
+    "https://pokeapi.co/api/v2/pokemon/4/"
+  );
 
   const imageRef = useRef<HTMLImageElement | null>(null);
 
@@ -27,10 +36,10 @@ export default function Game(): ReactElement {
         className={`guessPokemonImage `}
       />
       <div className="gameButtons">
-        <Button onClick={reveal} text="Pikachu" />
-        <Button text="Pikachu" />
-        <Button text="Pikachu" />
-        <Button text="Pikachu" />
+        <Button onClick={reveal} text={response.data?.name ?? ""} />
+        <Button text={response1.data?.name ?? ""} />
+        <Button text={response2.data?.name ?? ""} />
+        <Button text={response3.data?.name ?? ""} />
       </div>
     </div>
   );
