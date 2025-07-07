089610631f7fa4cc36cc339f9e34d39db8805001
MauricioLsalles <mauricio.leano@jala.university>
2025-07-07 16:49:49 -0400

    fix: add key

--- pokeapi/src/Molecules/HeaderLinks/HeaderLinks.tsx
+++ pokeapi/src/Molecules/HeaderLinks/HeaderLinks.tsx
@@ -4,18 +4,20 @@ type CustomProps = {
   links: Link[];
 };
 
-type Props = CustomProps & React.ButtonHTMLAttributes<HTMLDivElement>;
+type Props = CustomProps & React.ButtonHTMLAttributes<HTMLUListElement>;
 
 export default function HeaderLinks(props: Props) {
   const { links, ...ElementProps } = props;
   return (
-    <div
+    <ul
       style={{ display: "flex", gap: "3vw", alignItems: "center" }}
       {...ElementProps}
     >
-      {links.map((link: Link) => (
-        <span style={{ fontSize: "1.3rem" }}>{link.name}</span>
+      {links.map((link: Link, id: number) => (
+        <span key={id} style={{ fontSize: "1.3rem" }}>
+          {link.name}
+        </span>
       ))}
-    </div>
+    </ul>
   );
 }
