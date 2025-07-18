f35d1eb5f18e7769dd44889ce1fb80709fdcc6fa
MauricioLsalles <mauricio.leano@jala.university>
2025-07-18 16:13:52 -0400

    feat: created two organisms to for the PokemonInfoPage

---
+++ pokeapi/src/Organisms/PokemonDataOverview/PokemonDataOverview.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+import type { Pokemon } from "../../Types/Pokemon";
+
+export default function PokemonDataOverview({
+  pokemonData,
+}: {
+  pokemonData: Pokemon;
+}): ReactElement {
+  return <div></div>;
+}
