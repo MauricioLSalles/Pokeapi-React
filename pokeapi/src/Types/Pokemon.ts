829bc936a704a021f25ee178ac5b0d0f0ed42ca6
MauricioLsalles <mauricio.leano@jala.university>
2025-07-21 16:59:54 -0400

    feat: added types

--- pokeapi/src/Types/Pokemon.ts
+++ pokeapi/src/Types/Pokemon.ts
@@ -1,14 +1,42 @@
 import type { Label } from "./Label";
 
 export type Pokemon = {
-        id:number;
-        name:string;
-        types:Label[];
-        sprites:Sprites;
-}
-
+  id: number;
+  name: string;
+  types: Label[];
+  sprites: Sprites;
+  abilities: Abilitie[];
+  height: number;
+  weight: number;
+  stats: Stat[];
+};
 
 export type Sprites = {
-        back_default:string
-        front_default:string
-}
+  back_default: string;
+  front_default: string;
+};
+
+export type Abilitie = {
+  ability: AbilitieDetails;
+  is_hidden: boolean;
+  slot: number;
+};
+
+export type AbilitieDetails = {
+  name: string;
+};
+
+export type Stat = {
+  base_stat: number;
+  effort: number;
+  StatDetails: StatDetails;
+};
+
+export type StatDetails = {
+  name: string;
+};
+
+export type species = {
+  name: string;
+  url: string;
+};
