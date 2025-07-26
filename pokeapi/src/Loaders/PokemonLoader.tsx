98898d124c14b1e098b88b581a329e81ce118b79
MauricioLsalles <mauricio.leano@jala.university>
2025-07-25 21:05:35 -0400

    feat: updated loader of pokemon info page to load the extra data

--- pokeapi/src/Loaders/PokemonLoader.tsx
+++ pokeapi/src/Loaders/PokemonLoader.tsx
@@ -1,11 +1,44 @@
 import type { LoaderFunctionArgs } from "react-router";
 import ApiGet from "../CustomHooks/ApiGet";
-import type { Pokemon } from "../Types/Pokemon";
+import type { ExpandedDataPokemon, Pokemon } from "../Types/Pokemon";
 import type { Response } from "../Types/Response";
+import type { TypeExtendedDetails } from "../Types/TypeExtendedDetails";
+import type { SpeciesDetails } from "../Types/SpeciesDetails";
 
-export async function pokemonLoader({ params }: LoaderFunctionArgs) {
-  const response: Response<Pokemon> = await ApiGet<Pokemon>(
+export async function pokemonLoader({ params }: LoaderFunctionArgs): Promise<{
+  pokemonData: ExpandedDataPokemon;
+}> {
+  const pokemonResponse: Response<Pokemon> = await ApiGet<Pokemon>(
     `https://pokeapi.co/api/v2/pokemon/${params.id}/`
   );
-  return { pokemonResponse: response };
+  const typeDetailsResponse: Response<TypeExtendedDetails> =
+    await ApiGet<TypeExtendedDetails>(
+      pokemonResponse.data?.types[0].type.url ?? ""
+    );
+
+  const speciesResponse: Response<SpeciesDetails> =
+    await ApiGet<SpeciesDetails>(pokemonResponse.data?.species.url ?? "");
+
+  const expandedData = composePokemonData(
+    pokemonResponse.data,
+    typeDetailsResponse.data,
+    speciesResponse.data
+  );
+
+  return { pokemonData: expandedData };
+}
+
+function composePokemonData(
+  pokeData: Pokemon | null,
+  typeData: TypeExtendedDetails | null,
+  speciesData: SpeciesDetails | null
+): ExpandedDataPokemon {
+  if (pokeData !== null && typeData !== null && null !== speciesData) {
+    return {
+      ...pokeData,
+      ...typeData,
+      ...speciesData,
+    };
+  }
+  throw new Error();
 }
